8567f7aecbb1416c323e993607027b5d9c83f706
danielmeans <dameans@gmail.com>
2020-04-30 18:13:13 -0700

    Filled out Sample Trajectory methods

--- homework_fall2019-master/hw1/cs285/infrastructure/utils.py
+++ homework_fall2019-master/hw1/cs285/infrastructure/utils.py
@@ -8,7 +8,7 @@ import scipy
 def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('rgb_array')):
 
     # initialize env for the beginning of a new rollout
-    ob = TODO # HINT: should be the output of resetting the env
+    ob = env.reset()  # HINT: should be the output of resetting the env
 
     # init vars
     obs, acs, rewards, next_obs, terminals, image_obs = [], [], [], [], [], []
@@ -42,7 +42,7 @@ def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('
 
         # TODO end the rollout if the rollout ended 
         # HINT: rollout can end due to done, or due to max_path_length
-        rollout_done = TODO # HINT: this is either 0 or 1
+        rollout_done = 1 if done or steps >= max_path_length else 0  # HINT: this is either 0 or 1
         terminals.append(rollout_done)
         
         if rollout_done: 
@@ -61,11 +61,11 @@ def sample_trajectories(env, policy, min_timesteps_per_batch, max_path_length, r
     timesteps_this_batch = 0
     paths = []
     while timesteps_this_batch < min_timesteps_per_batch:
-        path = sample_trajectories(env=env,
-                                   policy=policy,
-                                   max_path_length=max_path_length,
-                                   render=render,
-                                   render_mode=render_mode)
+        path = sample_trajectory(env=env,
+                                 policy=policy,
+                                 max_path_length=max_path_length,
+                                 render=render,
+                                 render_mode=render_mode)
         timesteps_this_batch += get_pathlength(path)
         paths.append(path)
 
@@ -79,9 +79,13 @@ def sample_n_trajectories(env, policy, ntraj, max_path_length, render=False, ren
         Hint1: use sample_trajectory to get each path (i.e. rollout) that goes into paths
     """
     paths = []
-
-    TODO
-
+    while len(paths) < ntraj:
+        path = sample_trajectory(env=env,
+                                 policy=policy,
+                                 max_path_length=max_path_length,
+                                 render=render,
+                                 render_mode=render_mode)
+        paths.append(path)
     return paths
 
 ############################################
