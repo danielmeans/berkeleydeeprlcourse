ef088672352ecdaad8e0679e98cc0d902f83cdf4
danielmeans <dameans@gmail.com>
2020-05-04 15:25:58 -0700

    Update to init tf variables

--- homework_fall2019-master/hw1/cs285/infrastructure/rl_trainer.py
+++ homework_fall2019-master/hw1/cs285/infrastructure/rl_trainer.py
@@ -72,7 +72,7 @@ class RL_Trainer(object):
 
         ## TODO initialize all of the TF variables (that were created by agent, etc.)
         ## HINT: use global_variables_initializer
-        tf.global_variables_initializer()
+        self.sess.run(tf.global_variables_initializer())
 
 
     def run_training_loop(self, n_iter, collect_policy, eval_policy,
